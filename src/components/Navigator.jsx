566df9924917979119330f07f5ad4ce1feb67454
quarzizus <angelzizu06@gmail.com>
2021-06-10 18:35:08 -0500

    ADD -> About me

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -3,26 +3,50 @@ import { Link } from "react-router-dom";
 import "../styles/components/Navigator.scss";
 
 const Navigator = () => {
+  const handleOpen = () => {
+    const navigatorActive = document.querySelector(".Navigator-active");
+    const navigator = document.querySelector(".Navigator");
+    if (navigator.classList.contains("Block")) {
+      navigator.classList.replace("Block", "None");
+      navigatorActive.classList.replace("None", "Block");
+    }
+  };
+  const handleClose = () => {
+    const navigatorActive = document.querySelector(".Navigator-active");
+    const navigator = document.querySelector(".Navigator");
+    if (navigatorActive.classList.contains("Block")) {
+      navigatorActive.classList.replace("Block", "None");
+      navigator.classList.replace("None", "Block");
+    }
+  };
   return (
-    <nav className="Navigator">
-      <ul>
-        <li>
-          <Link to="/" className="Link">
-            About Me
-          </Link>{" "}
-        </li>
-        <li>
-          <Link to="/projects" className="Link">
-            Projects
-          </Link>
-        </li>
-        <li>
-          <Link to="/contactme" className="Link">
-            Contact me
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      {/* {!active ? ( */}
+      <button className="Navigator Block" onClick={handleOpen}>
+        X
+      </button>
+      {/* ) : ( */}
+      <nav className="Navigator-active None" onClick={handleClose}>
+        <ul>
+          <li>
+            <Link to="/" className="Link">
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" className="Link">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/contactme" className="Link">
+              Contact me
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      {/* )} */}
+    </>
   );
 };
 

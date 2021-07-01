e2dff7aa10bef5e56b75901bc3f9082cd3b3b31c
quarzizus <juanshow14@gmail.com>
2021-07-01 12:44:39 -0500

    ADD-> Favicon and useRef

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,35 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "../styles/components/Navigator.scss";
 
 const Navigator = () => {
+  const navActiveRef = useRef();
+  const navRef = useRef();
+
   const handleOpen = () => {
-    const navigatorActive = document.querySelector(".Navigator-active");
-    const navigator = document.querySelector(".Navigator");
-    if (navigator.classList.contains("Block")) {
-      navigator.classList.replace("Block", "None");
-      navigatorActive.classList.replace("None", "Block");
+    const navActive = navActiveRef.current;
+    const nav = navRef.current;
+    if (nav.classList.contains("Block")) {
+      nav.classList.replace("Block", "None");
+      navActive.classList.replace("None", "Block");
     }
   };
   const handleClose = () => {
-    const navigatorActive = document.querySelector(".Navigator-active");
-    const navigator = document.querySelector(".Navigator");
-    if (navigatorActive.classList.contains("Block")) {
-      navigatorActive.classList.replace("Block", "None");
-      navigator.classList.replace("None", "Block");
+    const navActive = navActiveRef.current;
+    const nav = navRef.current;
+    if (navActive.classList.contains("Block")) {
+      navActive.classList.replace("Block", "None");
+      nav.classList.replace("None", "Block");
     }
   };
   return (
     <>
-      <button className="Navigator Block" onClick={handleOpen} title="Menu">
+      <button
+        className="Navigator Block"
+        onClick={handleOpen}
+        title="Menu"
+        ref={navRef}
+      >
         <FontAwesomeIcon
           icon={faBars}
           style={{ color: "white", fontSize: "14px" }}
         />
       </button>
-      <nav className="Navigator-active None" onClick={handleClose}>
+      <nav
+        className="Navigator-active None"
+        onClick={handleClose}
+        ref={navActiveRef}
+      >
         <ul>
           <li>
             <Link to="/" className="Link">

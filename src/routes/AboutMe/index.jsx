3fe8d6c2e3b8421837297c75191f50555311b888
Miguel Vásquez <angelzizu06@gmail.com>
2024-06-24 23:16:16 -0500

    Se cambia el copy de la sección about me

--- src/routes/AboutMe/index.jsx
+++ src/routes/AboutMe/index.jsx
@@ -1,26 +1,28 @@
 import "./styles.scss";
 import Perfil from "../../images/Perfil.jpeg";
+import { Link } from "react-router-dom";
 
 const AboutMe = () => {
   return (
     <section className="AboutMe">
       <h2>
-        Hi !<br /> I am <span>Miguel Vásquez</span>
+        Hola
+        <br /> Mi nombre es <span>Miguel Vásquez</span>
       </h2>
       <picture>
         <img src={Perfil} alt="Quarzizus Miguel Vásquez" />
       </picture>
       <p>
-        I am frontend developer passionate for the technology, the music and the
-        education. I tell you ? Lets begin !
+        Me gustan los puzzles, el funk, los gatos y las nubes.
+        <br /> En mis ratos libres suelo tocar o componer música con mis amigos,
+        escribir cuentos, ver peliculas.
       </p>
-      <ul>
-        <li>To tell short histories</li>
-        <li>To play percussion</li>
-      </ul>
       <p>
-        I am currently learning Testing and Typescript to deep. In my
-        repositories you would see my progress.
+        Profesionalmente, me desempeño como desarrollador frontend desde hace
+        dos años. Aquí puedes conocer un poco de mi{" "}
+        <Link to={"/experiencia"} className="Link">
+          experiencia
+        </Link>
       </p>
     </section>
   );

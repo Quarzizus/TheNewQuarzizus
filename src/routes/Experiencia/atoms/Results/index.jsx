a1c765393f17a168f43e0d60d895ab23702cc987
Miguel Vásquez <angelzizu06@gmail.com>
2024-06-25 00:27:33 -0500

    Se añaden los resultados

--- src/routes/Experiencia/atoms/Results/index.jsx
+++ src/routes/Experiencia/atoms/Results/index.jsx
@@ -1,9 +1,20 @@
 import "./styles.scss";
+import { Items } from "./items";
 
 const Results = () => {
   return (
     <section className="Results">
       <p className="title">Resultados</p>
+      <div className="container">
+        {Items.map((item) => (
+          <a className="item" id={item.id} target="_blank" href={item.url}>
+            <picture>
+              <img src={item.image} alt={item.name} />
+            </picture>
+            <p>{item.name}</p>
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
